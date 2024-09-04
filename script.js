dbd887edffa0f278e2939e2c6a855818cdbdbc18
twinTails <twintailsofmobius@gmail.com>
2024-09-04 13:39:43 -0600

    Completed Step 83; learned about if...else if...else statements

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+// learn about if...else if...else statements
+if ("") {
+    console.log("Condition is true");
+} else if (5 < 10) {
+    console.log("5 is less than 10");
+} else {
+    console.log("This is the else block");
 }
 
 let result = ""
